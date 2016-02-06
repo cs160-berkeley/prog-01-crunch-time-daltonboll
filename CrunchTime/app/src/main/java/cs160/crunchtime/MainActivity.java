0f28488bdc0baced2ecd103adeb7b849e4f4c561
Dalton Boll <daltonboll@gmail.com>
2016-02-05 19:19:30 -0800

    Basic application functionality is complete

--- CrunchTime/app/src/main/java/cs160/crunchtime/MainActivity.java
+++ CrunchTime/app/src/main/java/cs160/crunchtime/MainActivity.java
@@ -8,14 +8,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Spinner;
+import android.widget.*;
 import java.util.*;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Initialize mapping of exercises to time/reps needed to burn 100 calories
+    // Initialize mapping of exercises to the number of reps/minutes needed to burn 100 calories
     public Map<String, Double> unitFor100Calories = new HashMap<String, Double>();
+    // Initialize mapping of exercises to "reps" or "minutes"
+    public Map<String, String> exerciseToDuration = new HashMap<String, String>();
+
+    // Static strings
+    public String REPS = "reps";
+    public String MINUTES = "minutes";
+
+    // Spinner drop down
+    Spinner exerciseSpinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,33 +42,83 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Set up the spinner listener for when a new exercise has been selected
+        exerciseSpinner = (Spinner) findViewById(R.id.exerciseSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.exercises, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        exerciseSpinner.setAdapter(adapter);
+
+        exerciseSpinner.setOnItemSelectedListener(
+                new OnItemSelectedListener() {
+                    public void onItemSelected(
+                            AdapterView<?> parent, View view, int position, long id) {
+                        onSelectExercise();
+                    }
+
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        // nothing
+                    }
+                });
+
         // set up our hash map
-        initializeCalorieHashMap();
+        initializeHashMaps();
     }
 
-    // Initialize our HashMap of exercises to units for 100 calories
-    public void initializeCalorieHashMap() {
+    // Initialize our exercise HashMaps
+    public void initializeHashMaps() {
+        String reps = "reps";
+
         unitFor100Calories.put("Situps", 200.0);
         unitFor100Calories.put("Pushups", 350.0);
         unitFor100Calories.put("Jumping Jacks", 10.0);
         unitFor100Calories.put("Jogging", 12.0);
+
+        exerciseToDuration.put("Situps", REPS);
+        exerciseToDuration.put("Pushups", REPS);
+        exerciseToDuration.put("Jumping Jacks", MINUTES);
+        exerciseToDuration.put("Jogging", MINUTES);
     }
 
     // Call when the user clicks the burn calories button
     public void onClickConvertToCalories(View view) {
-    System.out.println("BUTTON WAS CLICKED!");
-    TextView exercises = (TextView) findViewById(R.id.exercises);
-    exercises.setText("Let's burn some calories!");
-    Spinner exerciseSpinner = (Spinner) findViewById(R.id.exercise_spinner);
-    String selectedExercise = String.valueOf(exerciseSpinner.getSelectedItem());
-    System.out.println("The item we have selected is: " + selectedExercise);
+    TextView calorieNotification = (TextView) findViewById(R.id.caloriesBurned);
+    Spinner exerciseSpinner = (Spinner) findViewById(R.id.exerciseSpinner);
+        String exercise = String.valueOf(exerciseSpinner.getSelectedItem());
 
-    double caloriesBurned = getCalories(selectedExercise, 10, "reps");
-    exercises.setText("Calories Burned: " + String.valueOf(caloriesBurned));
+    EditText durationEntered = (EditText) findViewById(R.id.duration);
+        int duration = Integer.parseInt(durationEntered.getText().toString());
+
+    double caloriesBurned = getCalories(exercise, duration);
+    calorieNotification.setText("Calories Burned: " + String.valueOf(caloriesBurned));
+    }
+
+    // Call when the user selects a new exercise from the Spinner. Updates the text
+    // displayed next to the enter duration field
+    public void onSelectExercise() {
+        Spinner exerciseSpinner = (Spinner) findViewById(R.id.exerciseSpinner);
+        String exercise = String.valueOf(exerciseSpinner.getSelectedItem());
+        System.out.println("SELECTED EXERCISE = " + exercise);
+        String durationType = exerciseToDuration.get(exercise);
+
+        TextView enterDurationText = (TextView) findViewById(R.id.enterDuration);
+        String newText = enterDurationText.getText().toString();
+        System.out.println("newText is equal to " + newText);
+
+        if (durationType.equals(REPS)) {
+            newText = newText.replace(MINUTES, REPS);
+            System.out.println("newText is now: " + newText);
+        } else {
+            newText = newText.replace(REPS, MINUTES);
+            System.out.println("newText is now: " + newText);
+        }
+
+        enterDurationText.setText(newText);
     }
 
     // return the calculated calories burned from the specified exercise and duration
-    double getCalories(String exercise, int duration, String repsOrMinutes) {
+    double getCalories(String exercise, int duration) {
         double caloriesBurned = 0.0;
         double conversionFactorFor100Calories = unitFor100Calories.get(exercise);
         caloriesBurned = (duration / conversionFactorFor100Calories) * 100;
